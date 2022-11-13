95a29da7314f01026675a49ee6e787961e536b1e
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-13 20:08:13 +0700

    Array

---
+++ ARRAY/Program.cs
@@ -0,0 +1,47 @@
+﻿// ARRAY
+//Создание и вывод массива
+int[] CreateRandomArray(int N, int start, int end)//рандомный по параметрам
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] CreateArray()//заполняем руками
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array) //ввыод массива на экран
+{ 
+for (int i = 0; i < array.Length; i++)
+{
+   Console.Write(array[i] + " "); 
+}
+Console.WriteLine();
+}
+
+Console.Clear();
+Console.WriteLine("Введите колличество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myRandomArray = CreateRandomArray(num, min, max);
+ShowArray(myRandomArray);
+Console.WriteLine("=================");
+int[] myArray = CreateArray();
+ShowArray(myArray);
