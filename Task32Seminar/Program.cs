1414e27fac20dc9339a11e256f8d8f53d8025cde
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-13 20:42:45 +0700

    add task32

---
+++ Task32Seminar/Program.cs
@@ -0,0 +1,29 @@
+﻿// task32 Замена элементов массива, положительные элементы замените на соотвествующие отрицательные, и на оборот.
+int[] CreateRandomArray(int N, int start, int end)//рандомный по параметрам
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array) //ввыод массива на экран
+{ 
+for (int i = 0; i < array.Length; i++)
+{
+   Console.Write(array[i] + " "); 
+}
+Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(4, -9, 9);
+Console.WriteLine("Исходный массив:");
+ShowArray(myArray);
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = myArray[i] * -1;
+}
+Console.WriteLine("Негативный массив:");
+ShowArray(myArray);
