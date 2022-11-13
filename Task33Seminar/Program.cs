262b8d74e13e417dafe1b863477dae78892993e5
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-13 21:15:38 +0700

    add task33

---
+++ Task33Seminar/Program.cs
@@ -0,0 +1,37 @@
+﻿// task33 задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+
+int[] CreateRandomArray(int N, int start, int end)//рандомный по параметрам
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array) //ввыод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(12, -9, 9);
+Console.WriteLine("Массив:");
+ShowArray(myArray);
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool s = false;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] == num)
+    {
+        s = true;
+    }
+    
+}
+Console.WriteLine(s ? "Число присутствует в массиве!" : "Число НЕ присутствует в массиве!");
+
