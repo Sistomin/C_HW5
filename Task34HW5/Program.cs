ba1f7de3bffabb5db751c7de67d06b9c767f9b52
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-19 14:09:33 +0700

    add task34 hw5

---
+++ Task34HW5/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateRandomArray(int N, int start, int end)//рандомный по параметрам
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array) //ввыод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(15, 100, 999);
+Console.WriteLine("Массив:");
+ShowArray(myArray);
+int num = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    {
+        num++;
+    }
+}
+Console.WriteLine($"Колличество четных элементов в массиве: {num}");
